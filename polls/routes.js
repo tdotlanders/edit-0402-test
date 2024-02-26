e8037c6d868bc3a744c12ce27a31816da4b1142f
Marco Ventura <venturapm2000@gmail.com>
2024-02-26 23:00:42 +0000

    #3 half done

--- polls/routes.js
+++ polls/routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const schemas = require("./schemas");
 const services = require("./services");
-const { pollsCollection } = require("../db/mongodb");
+const { pollsCollection, getDB } = require("../db/mongodb");
 
 router.get("/", async (req, res) => {
   const polls = await services.getAllPolls();
@@ -24,14 +24,40 @@ router.post("/", async (req, res) => {
     return res.status(400).json(error.details);
   }
 
-  const db = await getDb();
+  const db = await getDB();
 
-  const insertRes = await db.collection(pollsCollection).insertOne(value);
+  const insertRes = await db.collection(pollsCollection).insertOne({
+    question: value.question,
+    options: value.options,
+  });
 
   const result = await services.getPollById(insertRes.insertedId);
 
   res.status(201).json(result);
 });
+
+router.put("/:id", async (req, res) => {
+  // validate body
+  const { error } = schemas.createPollSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json(error.details);
+  }
+  const pollId = req.params.id;
+
+  const poll = await services.getPollById(pollId);
+  if (!poll) {
+    return res.status(404).json({ error: "poll not found" });
+  }
+
+  // check poll has vote option
+  const { voteError } = schemas.voteSchema.validate(req.body);
+  if (voteError) {
+    return res.status(400).json(voteError.details);
+  }
+  res.status(200).json();
+});
+
+// update poll - increment vote count
 
 router.delete("/:id", async (req, res) => {
   const pollId = req.params.id;
