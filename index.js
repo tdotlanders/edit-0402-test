27a2659fc3f48256ee9ee11e5ababfd349c4b7e2
Marco Ventura <venturapm2000@gmail.com>
2024-03-07 17:48:19 +0000

    #5 done(?)

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./db/mongodb");
+const pino = require("pino");
+const expressPinoLogger = require("express-pino-logger");
 const auth = require("./auth/routes");
 const polls = require("./polls/routes");
 
 const app = express();
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
+const expressLogger = expressPinoLogger({ logger });
+app.use(expressLogger);
 app.use(express.json());
 app.use("/auth", auth);
 app.use("/polls", polls);
--- middleware.js
+++ middleware.js
@@ -3,10 +3,12 @@ const authServices = require("./auth/services");
 function auth(req, res, next) {
   const token = req.headers.authorization;
   if (!token) {
+    logger.warn("Authorization token missing");
     return res.status(401).json({});
   }
 
   if (!authServices.validateAccessToken(token)) {
+    logger.warn("Invalid access token");
     return res.status(401).json({});
   }
 
