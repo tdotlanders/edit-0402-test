dc1bd2b33d023f965a02cf1c1f463103b51ca810
Marco Ventura <venturapm2000@gmail.com>
2024-03-07 18:25:49 +0000

    #6 done(?)

--- middleware.js
+++ middleware.js
@@ -3,12 +3,10 @@ const authServices = require("./auth/services");
 function auth(req, res, next) {
   const token = req.headers.authorization;
   if (!token) {
-    logger.warn("Authorization token missing");
     return res.status(401).json({});
   }
 
   if (!authServices.validateAccessToken(token)) {
-    logger.warn("Invalid access token");
     return res.status(401).json({});
   }
 
